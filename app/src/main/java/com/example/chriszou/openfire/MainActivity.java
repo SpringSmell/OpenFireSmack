3e8625aafa33fd6b717b762de683cfb1ce6d3b19
chriszou <chrisSpringSmell@gmail.com>
2016-11-30 18:11:41 +0800

    chris | create chat room and join chat room and im message

--- app/src/main/java/com/example/chriszou/openfire/MainActivity.java
+++ app/src/main/java/com/example/chriszou/openfire/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ import android.widget.Toast;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
@@ -28,16 +30,27 @@ import org.jivesoftware.smack.chat.ChatManagerListener;
 import org.jivesoftware.smack.chat.ChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.sasl.provided.SASLPlainMechanism;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jivesoftware.smackx.muc.DiscussionHistory;
 import org.jivesoftware.smackx.muc.HostedRoom;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.MultiUserChatManager;
+import org.jivesoftware.smackx.muc.packet.GroupChatInvitation;
 import org.jivesoftware.smackx.vcardtemp.VCardManager;
 import org.jivesoftware.smackx.vcardtemp.packet.VCard;
+import org.jivesoftware.smackx.xdata.Form;
+import org.jivesoftware.smackx.xdata.FormField;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.acl.Group;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.net.SocketFactory;
 
@@ -56,9 +69,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button startConnectionBtn;
     private Button sendBtn;
 
+    private EditText registerAccount;
+    private EditText registerPassword;
+
     private ImageView imgIv;
 
     private EditText userNameEt;
+
+    private EditText chatRoomEt;
+
+    private MultiUserChat muc;
+    private EditText inputChatRoomEt;
 
     private String ipStr = "dev.cq196.cn";
     private int port = 5222;
@@ -100,6 +121,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         userNameEt = ( EditText ) findViewById ( R.id.userName );
         accountEt = ( EditText ) findViewById ( R.id.accountEt );
         passwordEt = ( EditText ) findViewById ( R.id.passwordEt );
+        registerAccount = ( EditText ) findViewById ( R.id.registerAccountEt );
+        registerPassword = ( EditText ) findViewById ( R.id.registerPasswordEt );
+        chatRoomEt = ( EditText ) findViewById ( R.id.chatRoomNickname );
+        inputChatRoomEt= ( EditText ) findViewById ( R.id.inputChatRoomMsg );
 
         ipEt.setText ( ipStr );
         portEt.setText ( port + "" );
@@ -108,6 +133,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById ( R.id.disconnectBtn ).setOnClickListener ( this );
         findViewById ( R.id.loadImg ).setOnClickListener ( this );
         findViewById ( R.id.loginBtn ).setOnClickListener ( this );
+        findViewById ( R.id.registerBtn ).setOnClickListener ( this );
+        findViewById ( R.id.createChatRoom ).setOnClickListener ( this );
+        findViewById ( R.id.joinChatRoom ).setOnClickListener ( this );
+        findViewById ( R.id.sendChatRoomMsg ).setOnClickListener ( this );
     }
 
     @Override public void onClick ( View view ) {
@@ -128,7 +157,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 disconnect ();
                 break;
             case R.id.loadImg:
-                loadImg ();
+                loadHeadImg ();
+                break;
+            case R.id.registerBtn:
+                new Thread () {
+
+                    @Override public void run () {
+
+                        try {
+                            createUser ( registerAccount.getText ().toString (),
+                                         registerPassword.getText ().toString () );
+                        } catch ( SmackException.NotConnectedException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        } catch ( XMPPException.XMPPErrorException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        } catch ( SmackException.NoResponseException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        } catch ( XMPPException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        } catch ( IOException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        } catch ( SmackException e ) {
+                            e.printStackTrace ();
+                            sendMessage ( e.getMessage (), "注册" );
+                        }
+                        super.run ();
+                    }
+                }.start ();
+                break;
+            case R.id.createChatRoom:
+                try {
+                    createGroupChat ( chatRoomEt.getText ().toString () );
+                } catch ( SmackException e ) {
+                    e.printStackTrace ();
+                    sendMessage ( e.getMessage (), "创建聊天群" );
+                } catch ( XMPPException.XMPPErrorException e ) {
+                    e.printStackTrace ();
+                    sendMessage ( e.getMessage (), "创建聊天群" );
+                }
+                break;
+            case R.id.joinChatRoom:
+                muc = joinChatRoom ( chatRoomEt.getText ().toString (),
+                                     accountEt.getText ().toString (),
+                                     "" );
+                muc.addMessageListener (
+                        new MessageListener () {
+
+                            @Override public void processMessage ( Message message ) {
+
+                                if ( !TextUtils.isEmpty ( message.getBody () ) ) {
+                                    sendMessage ( message.getBody (), message.getFrom () );
+                                }
+                            }
+                        } );
+                break;
+            case R.id.sendChatRoomMsg:
+                try {
+                    if(muc!=null) {
+                        muc.sendMessage ( "Hello client" );
+                    }else{
+                        sendMessage ( "加入房间失败","发送聊天室消息" );
+                    }
+                } catch ( SmackException.NotConnectedException e ) {
+                    sendMessage ( e.getMessage (),"发送聊天室消息" );
+                    e.printStackTrace ();
+                }
                 break;
         }
     }
@@ -155,11 +253,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void run () {
 
                 try {
-//                    if(connection.isConnected ()){
-//                        connection.connect ();
+//                    if ( connection == null ) {
+//                        connection = getConnection ( ipStr, port );
+//                    } else if ( connection != null ) {
+//                        connection.disconnect ();
 //                    }
-//                    SASLAuthentication.unBlacklistSASLMechanism ( "PLAIN" );
-//                    SASLAuthentication.blacklistSASLMechanism ( "DIGEST-MD5" );
+
+
+//                    SASLAuthentication.unBlacklistSASLMechanism("PLAIN");
+//                    SASLAuthentication.unBlacklistSASLMechanism("SCRAM-SHA-1");
+//
+//                    SASLAuthentication.blacklistSASLMechanism("PLAIN");
+//                    SASLAuthentication.blacklistSASLMechanism("SCRAM-SHA-1");
+//                    SASLAuthentication.blacklistSASLMechanism("DIGEST-MD5");
+//                    SASLPlainMechanism saslPlainMechanism=new SASLPlainMechanism ();
+//                    saslPlainMechanism.
+//                    SASLAuthentication.registerSASLMechanism (  );
                     connection.connect ();
                     connection.login ( account, password );
                     Presence presence = new Presence ( Presence.Type.available );
@@ -235,11 +344,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                                          }
                                                      }
                                                  } );
-//            Message msg = new Message ();
-//            msg.setBody ( msgStr );
-//            msg.setFrom ( "chris" );
-//            msg.setTo ( accountEt.getText ().toString ()+HOST );
-            chat.sendMessage ( msgStr );
+            Message msg = new Message ();
+            msg.setBody ( msgStr );
+            msg.setSubject ( "Test call " );
+            chat.sendMessage ( msg );
         } catch ( SmackException.NotConnectedException e ) {
             e.printStackTrace ();
             sendMessage ( e.getMessage (), "发送消息" );
@@ -249,12 +357,127 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void loadImg () {
+
+    private void createUser ( String account, String password ) throws SmackException, XMPPException, IOException {
+
+        if ( !connection.isConnected () ) {
+            connection.connect ();
+        }
+        AccountManager accountManager = AccountManager.getInstance ( connection );
+        accountManager.createAccount ( account, password );
+        accountManager.changePassword ( "1234" );
+//        accountManager.deleteAccount ();
+    }
+
+    /**
+     * 聊天并加入
+     *
+     * @param chatGroupNickname
+     *
+     * @throws SmackException
+     * @throws XMPPException.XMPPErrorException
+     */
+    private void createGroupChat ( String chatGroupNickname ) throws SmackException, XMPPException.XMPPErrorException {
+
+        MultiUserChatManager multiUserChatManager = MultiUserChatManager
+                .getInstanceFor ( connection );
+
+        List< HostedRoom > hostedRooms = multiUserChatManager
+                .getHostedRooms ( HOST.substring ( 1, HOST.length () ) );
+
+        MultiUserChat muc = multiUserChatManager
+                .getMultiUserChat (
+                        chatGroupNickname + "@conference." + connection.getServiceName () );
+        muc.create ( chatGroupNickname );
+        Form form = muc.getConfigurationForm ();
+        //克隆一个新表单
+        Form submitForm = form.createAnswerForm ();
+        // 向要提交的表单添加默认答复
+        List< FormField > fields = form.getFields ();
+        for ( int i = 0 ; fields != null && i < fields.size () ; i++ ) {
+            if ( FormField.Type.hidden != fields.get ( i ).getType () &&
+                    fields.get ( i ).getVariable () != null ) {
+                // 设置默认值作为答复
+                submitForm.setDefaultAnswer ( fields.get ( i ).getVariable () );
+            }
+        }
+        // 设置聊天室的新拥有者
+        List owners = new ArrayList ();
+        owners.add ( connection.getUser () );//用户JID
+        submitForm.setAnswer ( "muc#roomconfig_roomowners", owners );
+        // 设置房间名称
+        submitForm.setAnswer ( FormField.FORM_TYPE, "http://jabber.org/protocol/muc#roomconfig" );
+        //设置房间名称
+        submitForm.setAnswer ( "muc#roomconfig_roomname", "新的名称" );
+        //设置房间描述
+        submitForm.setAnswer ( "muc#roomconfig_roomdesc", "房间描述" );
+        //是否允许修改主题
+        submitForm.setAnswer ( "muc#roomconfig_changesubject", true );
+        //设置最大人数
+        List< String > maxusers = new ArrayList<> ();
+        maxusers.add ( "9" );
+        maxusers.add ( "19" );
+        submitForm.setAnswer ( "muc#roomconfig_maxusers", maxusers );
+        // 设置聊天室是持久聊天室，即将要被保存下来
+        submitForm.setAnswer ( "muc#roomconfig_persistentroom", true );
+        // 房间仅对成员开放
+        submitForm.setAnswer ( "muc#roomconfig_membersonly", true );
+        // 允许占有者邀请其他人
+        submitForm.setAnswer ( "muc#roomconfig_allowinvites", true );
+        // 能够发现占有者真实 JID 的角色
+        // submitForm.setAnswer("muc#roomconfig_whois", "anyone");
+        // 登录房间对话
+        submitForm.setAnswer ( "muc#roomconfig_enablelogging", true );
+        // 仅允许注册的昵称登录
+        submitForm.setAnswer ( "x-muc#roomconfig_reservednick", true );
+        // 允许使用者修改昵称
+        submitForm.setAnswer ( "x-muc#roomconfig_canchangenick", true );
+        // 允许用户注册房间
+        submitForm.setAnswer ( "x-muc#roomconfig_registration", true );
+        // 发送已完成的表单（有默认值）到服务器来配置聊天室
+        muc.sendConfigurationForm ( submitForm );
+        muc.join ( chatGroupNickname );
+    }
+
+    /**
+     * 加入一个群聊聊天室
+     *
+     * @param roomName 聊天室名字
+     * @param nickName 用户在聊天室中的昵称
+     * @param password 聊天室密码
+     *
+     * @return
+     */
+    public MultiUserChat joinChatRoom ( String roomName, String nickName, String password ) {
+
+        try {
+            // 使用XMPPConnection创建一个MultiUserChat窗口
+            MultiUserChat muc = MultiUserChatManager.getInstanceFor ( connection ).
+                    getMultiUserChat ( roomName + "@conference." + connection.getServiceName () );
+            // 聊天室服务将会决定要接受的历史记录数量
+            DiscussionHistory history = new DiscussionHistory ();
+            history.setMaxChars ( 0 );
+            // history.setSince(new Date());
+            // 用户加入聊天室
+            muc.join ( nickName, password );
+            return muc;
+        } catch ( XMPPException | SmackException e ) {
+            e.printStackTrace ();
+            sendMessage ( e.getMessage (), "加入聊天室" );
+            return null;
+        }
+    }
+
+    /**
+     * 加载头像图片
+     */
+    private void loadHeadImg () {
 
         VCardManager vCardManager = VCardManager.getInstanceFor ( connection );
         try {
-            VCard                vCard = vCardManager.loadVCard ( "admin" );
-            ByteArrayInputStream bais  = new ByteArrayInputStream ( vCard.getAvatar () );
+            VCard vCard = vCardManager
+                    .loadVCard ( accountEt.getText ().toString () );
+            ByteArrayInputStream bais = new ByteArrayInputStream ( vCard.getAvatar () );
 //                    imgIv.setImageDrawable ( Drawable.createFromStream ( bais, "image" ) );
             imgIv.setImageBitmap ( BitmapFactory.decodeStream ( bais ) );
         } catch ( SmackException.NoResponseException e ) {
@@ -277,13 +500,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         XMPPTCPConnectionConfiguration.Builder builder = XMPPTCPConnectionConfiguration.builder ();
         builder.setSocketFactory ( SocketFactory.getDefault () );
-        builder.setServiceName ( server );
+        builder.setServiceName ( HOST.substring ( 1, HOST.length () ) );
         builder.setHost ( server );
         builder.setPort ( port );
-        builder.setCompressionEnabled ( false );
+        builder.setCompressionEnabled ( false );//是否启用压缩
         builder.setDebuggerEnabled ( true );
-        builder.setSendPresence ( true );
-        builder.setSecurityMode ( ConnectionConfiguration.SecurityMode.disabled );
+        builder.setSendPresence ( true );//
+        builder.setSecurityMode ( ConnectionConfiguration.SecurityMode.disabled );//
         XMPPTCPConnection connection = new XMPPTCPConnection ( builder.build () );
 
         return connection;
@@ -313,7 +536,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         android.os.Message message1 = android.os.Message
                 .obtain ();
         message1.what = 1;
-        message1.obj = "收到消息：" + content +
+        message1.obj = content +
                 " 来自:" + from;
         mHandler.sendMessage ( message1 );
     }
